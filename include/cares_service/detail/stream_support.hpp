b1f00bbc38af8627b026b47eed1b996f09ec9a6f
Michał Papierski <michal@papierski.net>
2014-05-10 19:44:54 +0200

    Use portable ntop implementation.

--- include/cares_service/detail/stream_support.hpp
+++ include/cares_service/detail/stream_support.hpp
@@ -2,10 +2,14 @@
 #define CARES_SERVICE_DETAIL_OPERATORS_HPP_
 
 #include <ostream>
+#include <ares.h>
 
 std::ostream & operator<<(std::ostream & lhs, const struct ares_addrttl & rhs)
 {
-	return lhs << inet_ntoa(rhs.ipaddr);
+	char buf[1024] = {0};
+	const char * result = ares_inet_ntop(AF_INET, &(rhs.ipaddr.s_addr), buf, 1023);
+	assert(result == buf);
+	return lhs << result;
 }
 
 #endif
